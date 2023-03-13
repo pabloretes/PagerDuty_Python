f5f87a8a4fa1651238b2934f31cc0e5a56047d60
Pablo Retes <Pablo.Retes@gmail.com>
2023-03-13 17:19:13 -0300

    PagerDuty createTeams

--- createTeam.py
+++ createTeam.py
@@ -3,32 +3,32 @@ import json
 import csv
 import requests
 
-API_KEY = 'u+M1ooHmsW2rTsW7saCg'
 teamObjects = []
 
-with open("teamlist.csv", newline='') as csvTeams:
-    fileReader = csv.reader(csvTeams)
-    for teamData in fileReader:
-        name = teamData[0]
-        description = teamData[1]
-        teamObject: dict[str, str] = {
-            "type": "team",
-            "name": name,
-            "description": description
-        }
-        teamObjects.append(json.dumps(teamObject))
+def create_team(API_KEY):
+    with open("teamlist.csv", newline='') as csvTeams:
+        fileReader = csv.reader(csvTeams)
+        for teamData in fileReader:
+            name = teamData[0]
+            description = teamData[1]
+            teamObject: dict[str, str] = {
+                "type": "team",
+                "name": name,
+                "description": description
+            }
+            teamObjects.append(json.dumps(teamObject))
 
-        conn = http.client.HTTPSConnection("api.pagerduty.com")
-        payload = "{\n  \"team\": {\n    \"type\": \"team\",\n    \"name\": \"" + name + "\",\n    \"description\": \"" + description + "\"\n  }\n}"
+            conn = http.client.HTTPSConnection("api.pagerduty.com")
+            payload = "{\n  \"team\": {\n    \"type\": \"team\",\n    \"name\": \"" + name + "\",\n    \"description\": \"" + description + "\"\n  }\n}"
 
-        headers = {
-            'Content-Type': "application/json",
-            'Accept': "application/vnd.pagerduty+json;version=2",
-            'Authorization': f"Token token={API_KEY}"
-        }
+            headers = {
+                'Content-Type': "application/json",
+                'Accept': "application/vnd.pagerduty+json;version=2",
+                'Authorization': f"Token token={API_KEY}"
+            }
 
-        conn.request("POST", "/teams", payload, headers)
+            conn.request("POST", "/teams", payload, headers)
 
-        res = conn.getresponse()
-        data = res.read()
-        print(data.decode("utf-8"))
+            res = conn.getresponse()
+            data = res.read()
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import createUser
 import deleteUser
+import deleteTeam
+import createTeam
 import listTeams
 import listUsers
-import deleteTeam
+
 
 ############################
 # Author: Pablo Retes
@@ -12,24 +15,11 @@ API_KEY = 'u+M1ooHmsW2rTsW7saCg' # se reemplaza
 objTeams = listTeams.list_teams(API_KEY)
 objUsers = listUsers.list_users(API_KEY)
 
-######################################################################
-#Listar Teams
-print("\nListado de Teams Existentes:")
-for team in objTeams['teams']:
-    print(team["id"],team["name"])
-print(f" Se encontraron {len(objTeams['teams'])} Team(s)")
-
-#Listar Usuarios
-print("\nListado de Usuarios Existentes:")
-for user in objUsers['users']:
-    print(user["id"], user["name"])
-print(f" Se encontraron {len(objUsers['users'])} usuario(s)")
-
 #######################################################################
 #Eliminar Teams
 print("\nListado de Teams Eliminados:")
 for team in objTeams['teams']:
-    print(deleteTeam.delete_team(API_KEY, team["id"]))
+    print(f" {deleteTeam.delete_team(API_KEY, team['id'])}")
 print(f" Se eliminaron {len(objTeams['teams'])} Team(s)")
 
 #Eliminar Usuarios
@@ -38,10 +28,31 @@ for user in objUsers['users']:
     if user["id"] == "PP1VDH4":
         print(f" Este no se borra: {user['id']} {user['name']}")
     else:
-        print(deleteUser.delete_user(API_KEY, user["id"]),user["name"])
+        print(f" {deleteUser.delete_user(API_KEY, user['id'])}",user["name"])
 print(f" Se eliminaron {len(objUsers['users'])-1} usuario(s)")
 
 #######################################################################
 # Crear Teams
+createTeam.create_team(API_KEY)
+objTeams = listTeams.list_teams(API_KEY)
+print(f"\nSe crearon {len(objTeams['teams'])} Team(s)")
 
 # Crear Usuarios
+createUser.create_user(API_KEY)
+objUsers = listUsers.list_users(API_KEY)
+print(f"Se crearon {len(objUsers['users'])-1} Usuario(s)")
+
+######################################################################
+
+
+#Listar Teams
+print("\nListado de Teams Encontrados:")
+for team in objTeams['teams']:
+    print(f" {team['id']}",team['name'])
+print(f" Se encontraron {len(objTeams['teams'])} Team(s)")
+
+#Listar Usuarios
+print("\nListado de Usuarios Existentes:")
+for user in objUsers['users']:
+    print(f" {user['id']}, {user['name']}, {user['email']}")
+print(f" Se encontraron {len(objUsers['users'])} usuario(s)")
