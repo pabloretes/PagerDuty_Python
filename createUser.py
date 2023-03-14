df2aa54b6bc8c8601677295ae211706c3f3abe40
Pablo Retes <Pablo.Retes@gmail.com>
2023-03-13 22:48:59 -0300

    PagerDuty add user to team

--- createUser.py
+++ createUser.py
@@ -1,45 +1,42 @@
 import http.client
 import json
 import csv
+import requests
+import findTeam
 
-api_key = "u+M1ooHmsW2rTsW7saCg"
-# Email of the person running this script
-from_email = ""
+
+from_email = "sinclairchuli@proton.me"
 pagerduty_team_id = ""
 userObjects = []
+idTeam = ""
 
 def create_user(API_KEY):
+    url = 'https://api.pagerduty.com/users'
     with open("userslist.csv", newline='') as csvUsers:
         fileReader = csv.reader(csvUsers)
         for userData in fileReader:
-            name = userData[0]
-            email = userData[1]
-            title = userData[2]
-            team = userData[3]
-            userObject = {
-                "type": "user",
-                "name": name,
-                "email": email,
-                "job_title": title,
-                "Team_name": team,
-                "role": "limited_user",
+            headers = {
+                'Content-Type': "application/json",
+                'Accept': "application/vnd.pagerduty+json;version=2",
+                'From': from_email,
+                'Authorization': f'Token token={API_KEY}'
             }
-            userObjects.append(json.dumps(userObject))
 
-    conn = http.client.HTTPSConnection("api.pagerduty.com")
+            payload = {
+                'user': {
+                    'type': 'user',
+                    'name': userData[0],
+                    'email': userData[1]
+                }
+            }
 
-    headers = {
-        'Content-Type': "application/json",
-        'Accept': "application/vnd.pagerduty+json;version=2",
-        'From': from_email,
-        'Authorization': f'Token token={api_key}'
-    }
+            team = userData[3]
+            r = requests.post(url, headers=headers, data=json.dumps(payload))
+            print('Status Code: {code}'.format(code=r.status_code))
+            print(r.json())
 
-    for userObject in userObjects:
-        conn.request("POST", "/users", userObject, headers)
-        res = conn.getresponse()
-        data = json.loads(res.read())
-        payload = "{\n  \"role\": \"responder\"\n}"
-        conn.request("PUT", f'/teams/{pagerduty_team_id}/users/{data["user"]["id"]}', payload, headers)
-        res = conn.getresponse()
-        data2 = res.read()
+
+        if __name__ == '__main__':
+            create_user()
+
+
