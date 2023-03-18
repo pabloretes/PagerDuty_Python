539c1f149f7bb0792d804ccac9d2b78e0535fe50
Pablo Retes <Pablo.Retes@gmail.com>
2023-03-17 22:34:25 -0300

    PagerDuty adding Services

--- main_second.py
+++
@@ -1,8 +0,0 @@
-import listOnCallSchedule
-
-API_KEY = 'u+M1ooHmsW2rTsW7saCg' # se reemplaza
-
-############################################
-# To list Schedules
-############################################
-listOnCallSchedule.list_schedules(API_KEY)
